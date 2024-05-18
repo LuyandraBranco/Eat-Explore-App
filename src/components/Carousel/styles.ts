e4894bb90cf0e851fa3d80442a7fe6a4ed44efae
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2024-05-18 13:14:47 +0100

    refactor: Carousel melhorado e adicionado os dots

--- src/components/Carousel/styles.ts
+++ src/components/Carousel/styles.ts
@@ -6,26 +6,12 @@ export default StyleSheet.create({
     height: 160,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 15
+    borderRadius: 15,
   },
   carouselImage: {
     width: "100%",
     height: "100%",
     resizeMode: "cover",
-    borderRadius: 15
-  },
-  carouselDot: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    marginHorizontal: 8,
-    backgroundColor: "#F55F5F", 
-  },
-  carouselInactiveDot: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    marginHorizontal: 8,
-    backgroundColor: "#C1C1C1",
+    borderRadius: 15,
   },
 });
